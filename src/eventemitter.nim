fe1736e0fac6b2503ff829e4df020a6eda4a0b71
Huy Doan <me@huy.im>
2020-05-28 01:33:59 +0700

    support generic

--- src/eventemitter.nim
+++ src/eventemitter.nim
@@ -1,42 +1,47 @@
-type 
+type
   Args* = ref object of RootObj # ...args
   Handler = proc (args: Args) {.closure.} # callback function type
-  Event = tuple[name:string, handlers:seq[Handler]] # key value pair
-  EventEmitter = ref object
-    events: seq[Event] # will use sequence as fixed size array
-proc createEventEmitter(): EventEmitter = 
+  Event[T] = tuple[typ: T, handlers:seq[Handler]] # key value pair
+  EventEmitter[T] = ref object
+    events: seq[Event[T]] # will use sequence as fixed size array
+
+proc createEventEmitter*[T](): EventEmitter[T] =
   result.new
   result.events = @[]
-method on*(this: EventEmitter, name: string, handler: Handler): void {.base.} = 
-  var event: Event = (name: name, handlers: @[])
+
+method on*[T](this: EventEmitter[T], typ: T, handler: Handler): void {.base.} =
+  var event: Event[T] = (typ: typ, handlers: @[])
   var id: int = -1
   for i in 0..high(this.events):
-    if this.events[i].name == name:
+    if this.events[i].typ == typ:
       id = i
       event = this.events[i]
   event.handlers.add(handler)
   if id == -1: this.events.add(event)
   else: this.events[id] = event
-method once*(this:EventEmitter, name:string, handler:Handler): void {.base.} =
+
+method once*[T](this:EventEmitter[T], typ: T, handler:Handler): void {.base.} =
   var id: Natural = 0
   for i in 0..high(this.events):
-      if this.events[i].name == name:
+      if this.events[i].typ == typ:
         id = this.events[i].handlers.len()
-  this.on(name) do(a: Args):
+  this.on(typ) do(a: Args):
     handler(a)
     for i in 0..high(this.events):
-      if this.events[i].name == name:
+      if this.events[i].typ == typ:
         this.events[i].handlers.del(id)
-method emit*(this:EventEmitter, name:string, args:Args): void {.base.} =
+
+method emit*[T](this:EventEmitter[T], typ: T, args:Args): void {.base.} =
   for i in 0..high(this.events):
-    if this.events[i].name == name:
-      for x in 0..high(this.events[i].handlers): 
+    if this.events[i].typ == typ:
+      for x in 0..high(this.events[i].handlers):
         this.events[i].handlers[x](args)
+
 when isMainModule:
   block:
     type ReadyArgs = ref object of Args
       text: string
-    var evts = createEventEmitter()
+    var evts = createEventEmitter[string]()
     evts.on("ready") do(a: Args):
       var args = ReadyArgs(a)
       echo args.text, ": from [1st] handler"
